d4fe04003c367952b9a73232bef8bd0799d95132
Zach Mason <zacharycmason@gmail.com>
2017-08-01 15:28:47 -0700

    added document ready

--- ParkShare/Scripts/indexScript.js
+++ ParkShare/Scripts/indexScript.js
@@ -1,24 +1,20 @@
-﻿var button = $('.btn');
+﻿$(document).ready(function () {
+    var button = $('.btn');
 
-button.click(function () {
-    var spaces = $(this).html();
-    var amount = $("#" + spaces);
-    var amountInt = parseInt(amount.html());
+    button.click(function () {
+        console.log('click')
+        var spaces = $(this).html();
+        var amount = $("#" + spaces);
+        var amountInt = parseInt(amount.html());
 
-    if (this.className == "btn btn-primary") {
-        amountInt--;
-        amount.html(amountInt);
-        this.className = "btn btn-danger";
-    } else {
-        amountInt++;
-        amount.html(amountInt);
-        this.className = "btn btn-primary";
-    }
+        if (this.className == "btn btn-primary") {
+            amountInt--;
+            amount.html(amountInt);
+            this.className = "btn btn-danger";
+        } else {
+            amountInt++;
+            amount.html(amountInt);
+            this.className = "btn btn-primary";
+        }
+    });
 });
-
-function add(int) {
-    return int + 1;
-}
-function subtract(int) {
-    return int - 1;
-}
