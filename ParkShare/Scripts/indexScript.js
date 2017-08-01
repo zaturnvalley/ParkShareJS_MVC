8d26e0450f9f934a943be8760b7eae44a21cdcb0
Zach Mason <zacharycmason@gmail.com>
2017-08-01 13:37:54 -0700

    added calendar page (work in progress)

---
+++ ParkShare/Scripts/indexScript.js
@@ -0,0 +1,17 @@
+﻿var button = $('.btn');
+
+button.click(function () {
+    var spaces = $(this).text();
+    var amount = $("#" + spaces);
+    var spacesAvailable;
+
+    if (this.className == "btn btn-primary") {
+        spacesAvailable = amount.text() - 1;
+        amount.text(spacesAvailable);
+        this.className = "btn btn-danger";
+    } else {
+        spacesAvailable = amount.text() + 1;
+        amount.text(spacesAvailable);
+        this.className = "btn btn-primary";
+    }
+});
